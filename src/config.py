1224c489bd4eb91f74465e0e66e7725ba2eae229
Shreshth <shreshthdhimole@gmail.com>
2026-01-28 12:22:43 +0530

    Add 'Screenshot' to Images file category

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 # This file contains file type classification rules
 
 FILE_CATEGORIES = {
-    "Images": [".jpg", ".jpeg", ".png", ".gif"],
+    "Images": [".jpg", ".jpeg", ".png", ".gif","Screenshot"],
     "Videos": [".mp4", ".mkv", ".avi"],
     "Documents": [".pdf",".docx",".xlsx"],
     "Music": [".mp3", ".wav"],
